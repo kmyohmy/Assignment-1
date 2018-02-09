961387d2997f71f275cb2d07f46c735614cb5a95
kmyohmy <kennethmyatt@gmail.com>
2018-02-08 19:50:28 -0800

    Kenneth Myatt Assignment 1
    This was uploaded at 7:50

---
+++ js/script.js
@@ -0,0 +1,157 @@
+//HERE IS THE ASSIGNMENT
+
+/*
+// STEP 1
+var someMonth;
+function theMonth; 		// function to return current month
+currentMonth; 		    // a constant
+var summerMonth ; 		// an array of summer months
+function MyLibraryAFunction; 	    // a function 
+
+// STEP 2
+1.4                        //numeric literal expression
+"Hello Yall"                        //a string literal expression
+true                        //a Boolean literal expression
+null                      //a null literal expression.
+
+// STEP 3
+ var numComplexOne = 1 * (5+2) * 3;            //complex/variable expression 1
+ var numComplexOne = numComplexOne * 3;         //complex/variable expression 2
+
+// STEP 4
+
+var strFirstName;                                //First Name,
+var strLastName;                                 //Last Name, 
+var strAddress;                                  //Address, 
+var strCity;                                    //City, 
+var strState;                                   //State, 
+var numZipCode;                                //Zip Code,
+var numAge;                                    //Your Age, 
+var strReferralSource;                        //Referral Source, 
+var boolMayWeContactYou;                        //May We Contact You
+// STEP 5
+var strFirstName = document.getElementById('FirstName');     //Option 1
+var strReferralSource += document.getElementById('referred'); //Option 2
+
+var strAddress;                                              //Option 3
+strAddress = document.getElementById("Address");
+
+// STEP 6
+var numOne = 12;
+numOne += " is a number";
+window.console.log(numOne);
+
+var numTwo = "This is ";
+numTwo += false;
+window.console.log(numTwo);
+
+var numThree = 1;
+numThree += false;
+window.console.log(numThree);
+
+// STEP 7
+strHotelName = "Marriott";
+
+window.console.log("The hotel name is " + strHotelName);
+
+var strHotelName;
+
+
+// STEP 8
+
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."';         //Original Text
+window.console.log(someString);
+
+// NO THIS IS NOT RIGHT //
+
+var strsomeString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'; 
+
+^^^^^^^^THIS IS CORRECT ^^^^^^^^^^
+
+// STEP 9
+var emptyValue = null;                                //null value 
+window.console.log(emptyValue);
+var definitionNotFound;                               //undefined value 
+window.console.log(definitionNotFound);
+
+
+// STEP 10
+console.log(typeof "string");
+console.log(typeof 17);
+console.log(typeof  true);
+console.log(typeof {thang1:1,thang2:2});
+console.log(typeof variable);
+
+// STEP 11                   
+
+var strYaName=window.prompt("What is ya Name Bruh?");
+
+strCourseName= "Javascript";                        //Hoist This
+window.alert("Hello " + strYaName + ", \nWelcome to the " +strCourseName + " class!");
+
+// STEP 12
+var strYaName = "Kenneth M";
+
+// STEP 13
+var strCourseName= "Javascript";
+
+// STEP 14
+var strYaName=window.prompt("What is ya Name Bruh?");
+
+// STEP 15
+var strYaName=window.prompt("What is ya Name Bruh?");
+
+// STEP 16
+    //Okay, Did that. Please refer to Updated step 11.
+    
+// STEP 17
+
+var x = 10;
+var y = 20;
+window.console.log(x + y);
+
+// STEP 18
+var x = 20;
+x += 20;
+window.console.log(x);
+// STEP 19
+var x = 20;
+x *= 5;
+window.console.log(x);
+
+// STEP 20
+
+var x = 20;
+x /= 3;
+x /= 1;
+window.console.log(x);
+
+// STEP 21
+var boolcompareOne = 17;
+var boolcompareTwo = "17";
+
+if(boolcompareOne == boolcompareTwo){
+   console.log("We got a match!");
+   
+   }
+else {console.log("Nah bruh, They are not the same.")};
+
+// STEP 22
+
+if(boolcompareOne === boolcompareTwo){console.log("We got a perfect match!")}
+
+else {console.log("Nice Try. Either they kind of match, or they dont match at all")};
+
+// STEP 23
+var widget = new Object();
+window.console.log(typeof widget);
+
+// STEP 24
+var widget = new Object("Hello");
+window.console.log(widget instanceof Object);
+
+// STEP 25
+var widget = new Object("Hello");
+window.console.log(widget in Date);
+
+*/
